313293138b1822f38db78a0cd10119e03fc42547
Fabio Frasson <fabio.frass@gmail.com>
2021-04-14 21:56:45 -0300

    criação do projeto

---
+++ src/main/java/br/com/yolotech/cursospringudemy/CursoSpringUdemyApplication.java
@@ -0,0 +1,13 @@
+package br.com.yolotech.cursospringudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringUdemyApplication.class, args);
+	}
+
+}
